f86de212c43c2e30c4667421ec993bc34d42e6f1
putriaulia12-ux <114321719+putriaulia12-ux@users.noreply.github.com>
2022-09-26 07:36:59 +0700

    Add files via upload

---
+++ joptionpane.java
@@ -0,0 +1,11 @@
+package no.pkg2.pbo;
+
+import javax.swing.JOptionPane;
+public class joptionpane {
+    public static void main(String[] args) {
+        String nama ="";
+        nama = JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        String pesan = ( "belajar " + nama + " sangat mudah");
+        JOptionPane.showMessageDialog(null, pesan);        
+    }
+}
